94bbc30822b7930a609064f5c7e4ff257bf607f7
rishi-raj-jain <rishi18304@iiitd.ac.in>
2021-06-04 23:47:43 +0530

    Updated header design blog

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -18,46 +18,49 @@ const Snippet = ({ page }) => {
         description={page.excerpt}
         image={`${deploymentUrl}/${page.image}`}
       />
-      <div className="pt-8 flex flex-col items-center text-center space-y-8">
-        <h1 className="text-4xl sm:text-6xl text-black">{page.title}</h1>
-        <span className="text-gray-700 text-md">{`${
-          weekday[new Date(page.date).getDay()]
-        }, ${month[new Date(page.date).getMonth()]} ${new Date(
-          page.date
-        ).getDate()} ${new Date(page.date).getFullYear()}`}</span>
-        <span className="flex flex-row items-center space-x-5">
-          <div
-            className="w-[25px] h-[25px] border bg-gray-200 rounded-full"
-            style={{
-              backgroundImage: `url('${deploymentUrl}/ishant-jain.jpg')`,
-              backgroundSize: 'cover',
-            }}
-          />
-          <span className="flex flex-col items-center">
-            <span className="font-bold">Ishant Jain</span>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500"
-              href="https://twitter.com/ijainishant"
-            >
-              @ijainishant
-            </a>
+      <div className="pt-8 flex flex-col items-center text-center">
+        <div className="flex flex-col items-center w-[75vw] space-y-8">
+          <h1 className="text-4xl sm:text-6xl text-black">{page.title}</h1>
+          <span className="text-gray-700 text-md">{`${
+            weekday[new Date(page.date).getDay()]
+          }, ${month[new Date(page.date).getMonth()]} ${new Date(
+            page.date
+          ).getDate()} ${new Date(page.date).getFullYear()}`}</span>
+          <span className="flex flex-row items-center space-x-5">
+            <div className="relative w-[25px] h-[25px] border bg-gray-200 rounded-full">
+              <img
+                loading="lazy"
+                decoding="async"
+                src={`${deploymentUrl}/ishant-jain.jpg`}
+                className="absolute top-0 left-0 rounded-full"
+              />
+            </div>
+            <span className="flex flex-col items-center space-y-0">
+              <span className="font-bold">Ishant Jain</span>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500"
+                href="https://twitter.com/ijainishant"
+              >
+                @ijainishant
+              </a>
+            </span>
           </span>
-        </span>
-        <svg
-          onClick={() => scrollProjects('blog')}
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-          />
-        </svg>
+          <svg
+            onClick={() => scrollProjects('blog')}
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        </div>
       </div>
       <div
         id="blog"
