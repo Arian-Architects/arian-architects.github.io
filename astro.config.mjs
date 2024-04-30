58f53482af22396a839c89c322ffabcc4558eb96
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-04-30 22:43:48 +0530

    wide

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://arian-architects.github.io',
   integrations: [tailwind(), sitemap()],
+  site: 'https://arian-architects.github.io',
 })
