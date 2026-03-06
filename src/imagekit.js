c59fc551c3355bde734dbad75a153e6134bd1a10
Rishi <jain71000@gmail.com>
2026-03-07 00:28:10 +0530

    .

--- src/imagekit.js
+++ src/imagekit.js
@@ -3,20 +3,29 @@ import { join } from 'path'
 import ImageKit from 'imagekit'
 import { existsSync, readFileSync, writeFileSync } from 'fs'
 
-var imagekit = new ImageKit({
-  publicKey: process.env.PUBLIC_KEY,
-  privateKey: process.env.PRIVATE_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL,
-})
-
 export async function getImages() {
   const jsonPath = join(process.cwd(), 'src', 'cached_response.json')
   if (existsSync(jsonPath)) return JSON.parse(readFileSync(jsonPath, 'utf8'))
-  const tmp = await imagekit.listFiles({
+  // const imagekit = new ImageKit({
+  //   publicKey: process.env.PUBLIC_KEY,
+  //   privateKey: process.env.PRIVATE_KEY,
+  //   urlEndpoint: process.env.IMAGEKIT_URL,
+  // })
+  // const tmp = await imagekit.listFiles({
+  //   skip: 0,
+  //   limit: 999,
+  // })
+  // const arian = tmp.filter((i) => i.url.includes('/projects/'))
+  const arian = []
+  const imagekit_jain71 = new ImageKit({
+    publicKey: process.env.PUBLIC_KEY_JAIN71,
+    privateKey: process.env.PRIVATE_KEY_JAIN71,
+    urlEndpoint: process.env.IMAGEKIT_URL_JAIN71,
+  })
+  const tmp_jain71 = await imagekit_jain71.listFiles({
     skip: 0,
     limit: 999,
   })
-  const arian = tmp.filter((i) => i.url.includes('/projects/'))
-  writeFileSync(jsonPath, JSON.stringify(arian), 'utf8')
-  return arian
+  writeFileSync(jsonPath, JSON.stringify([...arian, ...tmp_jain71]), 'utf8')
+  return [...arian, ...tmp_jain71]
 }
