11528a9021414ee35112ea59eb5c9d0eff2598ca
Rishi <jain71000@gmail.com>
2026-03-07 01:11:53 +0530

    .

--- src/imagekit.js
+++ src/imagekit.js
@@ -6,16 +6,6 @@ import { existsSync, readFileSync, writeFileSync } from 'fs'
 export async function getImages() {
   const jsonPath = join(process.cwd(), 'src', 'cached_response.json')
   if (existsSync(jsonPath)) return JSON.parse(readFileSync(jsonPath, 'utf8'))
-  // const imagekit = new ImageKit({
-  //   publicKey: process.env.PUBLIC_KEY,
-  //   privateKey: process.env.PRIVATE_KEY,
-  //   urlEndpoint: process.env.IMAGEKIT_URL,
-  // })
-  // const tmp = await imagekit.listFiles({
-  //   skip: 0,
-  //   limit: 999,
-  // })
-  // const arian = tmp.filter((i) => i.url.includes('/projects/'))
   const imagekit_jain71 = new ImageKit({
     publicKey: process.env.PUBLIC_KEY_JAIN71,
     privateKey: process.env.PRIVATE_KEY_JAIN71,
